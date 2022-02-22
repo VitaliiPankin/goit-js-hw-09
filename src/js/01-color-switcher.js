45cacf11917b07ad66097af56d472b9d352af7bd
Vitalii Pankin <niknap777@ukr.net>
2022-02-22 11:48:19 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  const startBtn = document.querySelector('[data-start]');
+  
+  const stopBtn = document.querySelector("[data-stop]");
+  
+  startBtn.addEventListener("click", () => {
+      
+    timerId = setInterval((changeBgColor) => {
+        const color = getRandomHexColor();
+        document.body.style.background = color;
+    }, 1000);
+    startBtn.setAttribute('disabled', true);
+    
+  });
+
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled')
+  });
+
+//   disabled="true"
