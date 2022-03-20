7312d0779efc3d3338ce52a862f2a138649dfb6e
Vitalii Pankin <niknap777@ukr.net>
2022-03-20 20:20:09 +0200

    дз

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  useIcon: false,
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+
+      reject({ position, delay });
+    }, delay);
+  });
 }
+
+const refs = {
+  form: document.querySelector('.form')
+ }
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+  event.preventDefault();
+
+  const { delay, step, amount } = event.target;
+  let delayForPromise = Number(delay.value);
+  const stepForPromise = Number(step.value);
+  const amountForPromise = Number(amount.value);
+  
+    for (let i = 1; i <= amountForPromise; i+=1){
+    
+    createPromise(i, delayForPromise)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    delayForPromise += stepForPromise
+    
+  }
+      
+}
+
+
+
